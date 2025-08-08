0120f2c367abf7c8bd4ccd310bb5184d81ca0a87
YANZI-123 <1736433559@qq.com>
2025-08-08 12:44:59 +0800

    Site updated: 2025-08-08 12:44:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/30/网络管理实验/","2025/07/30/Hexo个人博客搭建教程/"];function toRandomPost(){
+var posts=["2025/07/30/网络管理实验/","2025/08/08/账户和组管理/","2025/07/30/Hexo个人博客搭建教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
