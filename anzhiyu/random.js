b7aa8425417b59241714d959d2384e6b6459b721
YANZI-123 <1736433559@qq.com>
2025-09-10 16:01:54 +0800

    Site updated: 2025-09-10 16:01:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/14/html 标签/"];function toRandomPost(){
+var posts=["2025/07/27/2025-7-27-vim编辑器/","2025/08/14/html 标签/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
